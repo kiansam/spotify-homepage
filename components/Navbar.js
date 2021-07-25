2c31576b15297bbb6d432eb36d13e0fc2749f887
Kian Sam <itskiansam@gmail.com>
2021-07-25 17:33:39 +0300

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function Navbar() {
+  return (
+    <>
+      <H1>Hello There</H1>
+    </>
+  );
+}
+
+export default Navbar;
+
+const H1 = styled.h1`
+  margin: 0;
+  text-align: center;
+  font-size: 5rem;
+`;
